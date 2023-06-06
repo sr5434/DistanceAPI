8bd3e307fc9c48ada10dc1303920e8e4e2893682
Samir <118690585+sr5434@users.noreply.github.com>
2023-06-06 10:18:46 -0400

    Add types in index.ts

--- supabase/functions/distance-calculator/index.ts
+++ supabase/functions/distance-calculator/index.ts
@@ -1,10 +1,6 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 
-function calc_dist(lat1, lat2, lon1, lon2){
+function calc_dist(lat1: number, lat2: number, lon1: number, lon2: number){
   // The math module contains a function
   // named toRadians which converts from
   // degrees to radians.
@@ -39,9 +35,4 @@ serve(async (req) => {
     { headers: { "Content-Type": "application/json" } },
   )
 })
-
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+export {calc_dist};
