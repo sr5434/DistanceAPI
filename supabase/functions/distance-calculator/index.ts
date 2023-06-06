894568cff14e2c5cad65437b62ebc142e1f23f35
Samir <118690585+sr5434@users.noreply.github.com>
2023-06-06 10:12:14 -0400

    Fix lint issues

--- supabase/functions/distance-calculator/index.ts
+++ supabase/functions/distance-calculator/index.ts
@@ -14,14 +14,14 @@ function calc_dist(lat1, lat2, lon1, lon2){
   lat2 = lat2 * Math.PI / 180;
 
   // Haversine formula
-  let dlon = lon2 - lon1;
-  let dlat = lat2 - lat1;
-  let a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2),2);
-  let c = 2 * Math.asin(Math.sqrt(a));
+  const dlon = lon2 - lon1;
+  const dlat = lat2 - lat1;
+  const a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2),2);
+  const c = 2 * Math.asin(Math.sqrt(a));
 
   // Radius of earth in kilometers. Use 3956
   // for miles
-  let r = 6371;
+  const r = 6371;
 
   // calculate the result
   return(c * r);
